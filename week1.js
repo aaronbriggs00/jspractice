808bf50ceb6baa7ef09aceb8d544b61d4b668078
AaronBriggs <aaron00.briggs@gmail.com>
2021-10-02 16:08:46 -0500

    week1

--- week1.js
+++ week1.js
@@ -11,4 +11,38 @@ function arrayPrinter(array) {}
 // should print every item in array 1
 arrayPrinter(testArray1);
 
-// 3.  write a function that rounds every number to the enearest integer
+// 3.  write a function that rounds every number to the nearest integer
+var roundingArray = [1.22, 3.59, 2.08, -6.97, 4.59];
+
+function arraryRounder(array) {}
+
+// should print 1 4 2 -7 5
+arraryRounder(roundingArray);
+
+// 4. write a function that returns the sum of every number in the array
+var summingArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function summation() {}
+
+// should return 45
+summation(summingArray);
+
+// 5. write a function that makes a numbered to-do list out of the an array of chores
+
+var chores = [
+  "feed the dog",
+  "clean bathroom",
+  "meal prep for the week",
+  "check email",
+  "get some rest",
+];
+
+function toDoListGen() {}
+
+// should print in the console
+// 1. feed the dog
+// 2. clean bathroom
+// 3. meal prep for the week
+// 4. check email
+// 5. get some rest
+toDoListGen(chores);
