eae9282875f82040b4486744898d09fe71e8da04
AaronBriggs <aaron00.briggs@gmail.com>
2021-10-02 16:03:18 -0500

    init

---
+++ week1.js
@@ -0,0 +1,14 @@
+//1.  write a function that converts farenheight to celsius
+function tempConvert(num) {}
+// should output 0
+tempConvert(32);
+
+//2.  write a function that prints every item in an array
+var testArray1 = [1, 3, -9, 0, "some string"];
+
+function arrayPrinter(array) {}
+
+// should print every item in array 1
+arrayPrinter(testArray1);
+
+// 3.  write a function that rounds every number to the enearest integer
